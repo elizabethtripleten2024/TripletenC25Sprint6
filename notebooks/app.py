ca0d5d57d4a4803357682ec6cce812a81a52b890
elizabeth <merycastillo2021@gmail.com>
2024-11-10 17:22:52 -0500

    Actualización del archivo app.py

--- notebooks/app.py
+++ notebooks/app.py
@@ -3,7 +3,9 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
 st.header('analisis de archivo vehiculos proyecto sprint 6')
+
 hist_button = st.button('Construir histograma') # crear un botón
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +16,20 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+
+build_scatter = st.checkbox('Construir un scatter') # crear un botón
+if build_scatter: # al hacer clic en el botón
+    # escribir un mensaje
+     st.write('Creación de un scatterplot')
+            
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
